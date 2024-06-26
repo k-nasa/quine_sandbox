a02a0e205233e6dd20596e58076b68a27bc0e8c0
k-nasa <htilcs1115@gmail.com>
2024-06-26 15:39:04 +0900

    update

---
+++ intro.rb
@@ -0,0 +1,22 @@
+require 'json'
+
+self_introduction = {
+  name: "nasa",
+  age: 25,
+  hobby: "reading books",
+  job: "engineer",
+  history: [
+    {
+      name: "STORES, Inc.",
+      job_description: "バックエンド中心にあれこれ",
+      since: "2024-04-15",
+    },
+    {
+      "name": "Wantedly, Inc.",
+      "job_description": "推薦基盤をあれこれ",
+      since: "2021-04-01".."2024-02-29",
+    },
+  ]
+}
+
+puts "#{self_introduction.to_json}"
